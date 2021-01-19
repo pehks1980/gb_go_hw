f265183f88e36735dde85afc436207fd01b28b32
pehks1980 <pehks1980@gmail.com>
2021-01-19 22:08:57 +1100

    home work урока 2 app1-3 задачки по списку

---
+++ hw2/app2/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pehks1980/gb_go_hw/hw2/app2/circle"
+	"os"
+)
+
+/*Напишите программу, вычисляющую диаметр(наверное, скорее радиус) и длину окружности по заданной площади круга.
+	Площадь круга должна вводиться пользователем с клавиатуры.*/
+func main() {
+	var circle_area float64
+
+	fmt.Printf("Введите, пожалуйста площадь круга: ")
+
+	_, err := fmt.Scanf("%g", &circle_area)
+	if err != nil {
+		// error here
+		fmt.Printf("Error enter")
+		os.Exit(128)
+	}
+	// compute radius & circle_length in one func
+	diameter, circle_len := circle.CircleParams(circle_area)
+
+	fmt.Printf("Диаметр окружности равен: %.2f\n", diameter )
+	fmt.Printf("Длина окружности равна: %.2f\n", circle_len)
+}
---
+++ hw2/app3/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+/*  С клавиатуры вводится трехзначное число.
+    Выведите цифры, соответствующие количество
+	сотен, десятков и единиц  в этом числе.
+ */
+
+func main() {
+	var aNumber int16
+
+	fmt.Printf("Введите, пожалуйста число:")
+
+	_, err := fmt.Scanf("%d", &aNumber)
+	if err != nil {
+		// error here
+		fmt.Printf("Error enter")
+		os.Exit(128)
+	}
+
+	fmt.Printf("Число сотен: %d, десятков: %d, единиц: %d\n",
+		(aNumber % 1000) / 100, (aNumber % 100) / 10 , aNumber % 10 )
+}
