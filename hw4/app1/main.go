eb2d1c48704d166f378c110d80cd66c5682be068
pehks1980 <pehks1980@gmail.com>
2021-01-28 09:54:49 +1100

    homework4 simple sort

---
+++ hw4/app1/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+//функция сортирует список int
+func mySortIns(a []int) []int {
+	// движемся от 2 элемента в массиве до конца массива
+	for i := 1; i < len(a); i++ {
+		// берем сравнваемое значение из массива
+		tmp := a[i]
+		idx := i
+		// тут (idx - i) кусок в который будет вставляться tmp
+		// в цикле идет раздвижение (на одну позицию вправо) в этом куске до того места в который
+		// будет вставлена tmp по ранжиру
+		for idx > 0 && a[idx-1] > tmp {
+			a[idx] = a[idx-1]
+			idx--
+		}
+		a[idx] = tmp
+		// в одной итерации цикла for кусок массива (idx - i) - отсортирован
+	}
+	return a
+}
+
+func main() {
+
+	s1 := []int{5, 2, 6, 3, 1, 4} // unsorted
+	//sort.Sort(sort.Reverse(sort.IntSlice(s1)))
+	fmt.Println(s1)
+
+	s2 := mySortIns(s1)
+
+	fmt.Println(s2)
+
+}
+
+
